4366d70e3052d0f65b7fc847f1408bf94d9f95af
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-05-24 15:57:54 -0400

    Basic class definations
    I have provided some basic classes based on the blue prints we went discussed during the last standup

---
+++ RestfulGroup3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group_3.restful_group_3_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulGroup3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulGroup3ProjectApplication.class, args);
+	}
+
+}
---
+++ User.java
@@ -0,0 +1,5 @@
+package com.group_3.restful_group_3_project;
+
+public class User {
+
+}
