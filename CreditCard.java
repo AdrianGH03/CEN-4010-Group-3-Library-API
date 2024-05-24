81294da23758060505b8627574192563ac7a8851
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-05-24 16:03:25 -0400

    Update CreditCard.java
    incorrect info was loaded in

--- CreditCard.java
+++ CreditCard.java
@@ -2,22 +2,6 @@ package com.group_3.restful_group_3_project;
 
 public class CreditCard {
 
-  String userID;
-	int starRating;
-	String comment;
-	String IBSN;
-	
-	public Rating() {}
-	
-	public Rating(String userID, int starRating, String comment, String iBSN) {
-		super();
-		this.userID = userID;
-		this.comment = comment;
-		this.starRating = starRating;
-		IBSN = iBSN;
-	}
-	
-	
 
 
 }
