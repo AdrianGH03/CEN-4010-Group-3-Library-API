650dff80be50716d11c84217989fe713113d7ace
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-05-24 16:04:33 -0400

    Update User.java
    provided vars and constructors

--- User.java
+++ User.java
@@ -1,5 +1,48 @@
 package com.group_3.restful_group_3_project;
 
 public class User {
+  String username;
+	String password;
+	String userID;
+	CreditCard creditCard;
+	
+	public User() {}
+		
+	
+	
+	public User(String username, String password, String userID, CreditCard creditCard) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.userID = userID;
+		this.creditCard = creditCard;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+	
+	
+	
 
 }
