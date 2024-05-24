ae8ff21eefc968fcbc67f79797644ba65166d964
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-05-24 16:03:51 -0400

    Update Rating.java
    provided vars and constructors

--- Rating.java
+++ Rating.java
@@ -1,5 +1,22 @@
 package com.group_3.restful_group_3_project;
 
 public class Rating {
+  String userID;
+	int starRating;
+	String comment;
+	String IBSN;
+	
+	public Rating() {}
+	
+	public Rating(String userID, int starRating, String comment, String iBSN) {
+		super();
+		this.userID = userID;
+		this.comment = comment;
+		this.starRating = starRating;
+		IBSN = iBSN;
+	}
+	
+	
+
 
 }
