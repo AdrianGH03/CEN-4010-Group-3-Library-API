c326b52eec0d547151eada80efea1291cff76dbe
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-05-29 12:00:21 -0400

    Update Rating.java
    provided validation in the rating class. To ensure that users provide ratings between 1 and 5

--- Rating.java
+++ Rating.java
@@ -1,22 +1,43 @@
 package com.group_3.restful_group_3_project;
+import java.time.LocalDate;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 public class Rating {
-  String userID;
-	int starRating;
-	String comment;
-	String IBSN;
+	private LocalDate ratedOn;
+	private String userID;
+	
+	@Min(1) 
+	@Max(5)
+	private int starRating;
 	
 	public Rating() {}
 	
-	public Rating(String userID, int starRating, String comment, String iBSN) {
+	public Rating(LocalDate ratedOn, String userID, int starRating) {
 		super();
 		this.userID = userID;
-		this.comment = comment;
 		this.starRating = starRating;
-		IBSN = iBSN;
+		this.ratedOn = ratedOn;
+	}
+
+	public String getUserID() {
+		return userID;
 	}
 	
+	public int getStarRating() {
+		return starRating;
+	}
+
+	public void setStarRating(int starRating) {
+		this.starRating = starRating;
+	}
+
+	public LocalDate getRatedOn() {
+		return ratedOn;
+	}
+    
+	public void setRatedOn(LocalDate ratedOn) {
+		this.ratedOn = ratedOn;
+	}
 	
-
-
 }
