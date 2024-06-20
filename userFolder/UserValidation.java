33ceecdcfe14a59cb96d174897364b20a71ac64d
Miguel Gomez <miguelgomezca2021@gmail.com>
2024-06-20 15:23:14 -0400

    Adding validation for inputs inside user profile requests

---
+++ userFolder/UserValidation.java
@@ -0,0 +1,22 @@
+package com.group_3.restful_group_3_project.userFolder;
+
+public class UserValidation {
+    
+    public static boolean isValidEmail(String email) {
+        if (email == null || email.isEmpty()) return false;
+
+        int atPosition = email.indexOf('@');
+        int dotPosition = email.lastIndexOf('.');
+        
+        return atPosition > 0 && dotPosition > atPosition + 1 && dotPosition < email.length() - 1; //check if email matches <useremailname>@<email>.<domain>
+    }
+
+    public static boolean isValidUsername(String username) {
+        return username != null && !username.trim().isEmpty();
+    }
+
+    public static boolean isValidPassword(String password) {
+        //can add more in future like 1 capital letter, 1 number, 1 special character etc. depends.
+        return password != null && password.length() >= 8;
+    }
+}
