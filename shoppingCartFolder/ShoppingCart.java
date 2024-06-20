97b1487bcddec9879e90754e26cb30fb192cc5b2
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-06-20 17:12:04 -0500

    Update ShoppingCart.java

--- shoppingCartFolder/ShoppingCart.java
+++ shoppingCartFolder/ShoppingCart.java
@@ -1,22 +1,54 @@
-package com.group_3.restful_group_3_project.shoppingCartFolder;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+package shoppingCartFolder;
 
 import java.util.List;
-
 import org.springframework.data.annotation.Id;
-
+import org.springframework.data.mongodb.core.mapping.Document;
 import com.group_3.restful_group_3_project.bookFolder.Book;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Data // Lombok annotation to create all the getters, setters, equals, hash, and toString methods
-@NoArgsConstructor // Lombok annotation to create a no args constructor
-@AllArgsConstructor // Lombok annotation to create a all args constructor
-
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Document(collection = "shopping_carts")
 public class ShoppingCart {
-    @Id  // Annotation to specify the primary key of the document
-    private String id;
+    @Id
+    private String userID;
     private double subTot;
-    private List<Book> book;
+    private List<Book> books;
+
+    public ShoppingCart(String userID, double subTot, List<Book> books) {
+        this.userID = userID;
+        this.subTot = subTot;
+        this.books = books;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public double getSubTot() {
+        return subTot;
+    }
+
+    public void setSubTot(double subTot) {
+        this.subTot = subTot;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public void addItem(Book book) {
+        books.add(book);
+    }
 }
