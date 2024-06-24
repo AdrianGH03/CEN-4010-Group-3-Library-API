4974aaa4ad356bd2deb527de729c7bc4a806c211
christ082000 <christ082000@gmail.com>
2024-06-24 19:52:12 -0400

    BookRepository.java

--- bookFolder/BookRepository.java
+++ bookFolder/BookRepository.java
@@ -4,7 +4,6 @@ package com.group_3.restful_group_3_project.bookFolder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // Annotation to specify that this class is a repository, which connects to the database
+@Repository
 public interface BookRepository extends MongoRepository<Book, String> {
-	 Book findByIsbn(String isbn);
 }
