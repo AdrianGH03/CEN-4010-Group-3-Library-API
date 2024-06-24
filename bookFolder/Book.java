1a0c5dcf5ad488b0952dabca5a8e54edf5e08a0a
christ082000 <christ082000@gmail.com>
2024-06-24 19:49:58 -0400

    Book.java

--- bookFolder/Book.java
+++ bookFolder/Book.java
@@ -9,27 +9,123 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.group_3.restful_group_3_project.reviewFolder.Rating;
 
-@Data // Lombok annotation to create all the getters, setters, equals, hash, and toString methods
-@NoArgsConstructor // Lombok annotation to create a no args constructor
-@AllArgsConstructor // Lombok annotation to create a all args constructor
-@Document(collection = "books") // Annotation to specify the collection name in MongoDB
+@Document(collection = "bookdetails")
 public class Book {
-    @Id  // Annotation to specify the primary key of the document
-    private String isbn; 
 
-    private String title;
+    @Id
+    private String isbn;
+
+    private String name;
+    private String description;
+    private double price;
+    private String author;
     private String genre;
     private String publisher;
-    private double price;
-    private int copiesSold;
-    private List<Comment> comments;
-    private List<Rating> ratings;
     private int yearPublished;
-    private String author;
-    private String description;
+    private int copiesSold;
 
+    public Book() {
+    }
 
-    public void addRating(Rating rating) {
-        ratings.add(rating);
+    public Book(String isbn, String name, String description, double price, String author, String genre,
+                String publisher, int yearPublished, int copiesSold) {
+        this.isbn = isbn;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.yearPublished = yearPublished;
+        this.copiesSold = copiesSold;
+    }
+
+    
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public int getCopiesSold() {
+        return copiesSold;
+    }
+
+    public void setCopiesSold(int copiesSold) {
+        this.copiesSold = copiesSold;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", yearPublished=" + yearPublished +
+                ", copiesSold=" + copiesSold +
+                '}';
     }
 }
