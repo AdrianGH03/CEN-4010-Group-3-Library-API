233eac78f3a44521b0c9ab49172354a101b557f1
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-06-27 14:32:19 -0400

    Update Rating.java
    updating Rating class to inherit from abstract class Review

--- reviewFolder/Rating.java
+++ reviewFolder/Rating.java
@@ -1,18 +1,23 @@
 package com.group_3.restful_group_3_project.reviewFolder;
 
-import java.time.LocalDate;
-import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 
 @Data // Lombok annotation to create all the getters, setters, equals, hash, and toString methods
 @NoArgsConstructor // Lombok annotation to create a no args constructor
 @AllArgsConstructor // Lombok annotation to create a all args constructor
-public class Rating {
-   
-    private String userID;
-    private LocalDate ratedOn;
-    private int starRating;
+@EqualsAndHashCode(callSuper = true)
+
+public class Rating extends Review {
+	
+	@Min(1) 
+	@Max(5)
+	private int starRating;
+		
 }
