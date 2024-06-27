98105d93ab80a7bc5d5b22c3aa8a60acd2d28b67
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-06-27 14:32:55 -0400

    Update Comment.java
    updating comment class to inherit from abstract class

--- reviewFolder/Comment.java
+++ reviewFolder/Comment.java
@@ -1,18 +1,17 @@
 package com.group_3.restful_group_3_project.reviewFolder;
 
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 
 @Data // Lombok annotation to create all the getters, setters, equals, hash, and toString methods
 @NoArgsConstructor // Lombok annotation to create a no args constructor
 @AllArgsConstructor // Lombok annotation to create a all args constructor
+@EqualsAndHashCode(callSuper = true)
 
-public class Comment {
-	private LocalDate ratedOn;
-	private String userID;
+public class Comment extends Review {
 	private String comment;
 	
 }
