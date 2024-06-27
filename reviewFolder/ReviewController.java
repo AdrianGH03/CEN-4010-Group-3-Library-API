25ea22a4bfe525113c0257277aa1ed7f1bde427d
codeWithDez <105659216+janaiie@users.noreply.github.com>
2024-06-27 16:32:07 -0400

    Update ReviewController.java
    renamed bookService to reviewService
    Set up end points for getAllComments

--- reviewFolder/ReviewController.java
+++ reviewFolder/ReviewController.java
@@ -1,28 +1,57 @@
 package com.group_3.restful_group_3_project.reviewFolder;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Annotation to specify that this class is a controller and will hold the endpoints (CRUD operations)
+
+import com.group_3.restful_group_3_project.bookFolder.Book;
+
+import jakarta.validation.Valid;
+
+@RestController
 public class ReviewController {
-
-    private ReviewService bookService;
-    
-    public ReviewController(ReviewService bookService) {  //constructor injection
-		this.bookService = bookService;
-	}
-        
-    @PostMapping(path="books/{isbn}/rate") // Endpoint to rate a book (i.e. http://localhost:8080/books/1234567890123/rate)
-   public void rateBook(@PathVariable String isbn,@Valid @RequestBody Rating rating) {
-		bookService.rateBookService(isbn,rating);
-	}
-
-    @PostMapping(path="books/{isbn}/comment") // Endpoint to rate a book (i.e. http://localhost:8080/books/1234567890123/comment)
-	public void leaveComment(@PathVariable String isbn,@RequestBody Comment comment) {
-		bookService.leaveCommentService(isbn,comment);
 	
-    
+	
+private ReviewService reviewService;
+	
+	public ReviewController(ReviewService reviewService) {
+		this.reviewService = reviewService;
+	}
+			
+	
+	@GetMapping(path="books/{isbn}")
+	public Book retrieveBook(@PathVariable String isbn) {
+		return reviewService.findByIsbn(isbn);
+	}
+
+	@PostMapping(path="books/{isbn}/rate")
+	public void rateBook(@PathVariable String isbn,@Valid @RequestBody Rating rating) {
+		reviewService.rateBookService(isbn,rating);
+	}
+	
+	@PostMapping(path="books/{isbn}/comment")
+	public void leaveComment(@PathVariable String isbn,@RequestBody Comment comment) {
+		reviewService.leaveCommentService(isbn,comment);
+	}
+	
+	@GetMapping(path="books/{isbn}/getComments")
+	public ResponseEntity<?> getAllComments(@PathVariable String isbn){
+		List<Comment> comments = reviewService.retrieveCommentsService(isbn);
+		
+		if (!comments.isEmpty()) {
+			return ResponseEntity.ok(comments);
+		}
+		 return ResponseEntity.status(HttpStatus.NO_CONTENT).body("This book has no comments");
+	 }
+	
+
+	
 }
+
