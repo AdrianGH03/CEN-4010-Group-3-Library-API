e521cc747552b80a8c48db8e41b9ae6665d905ec
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-07-06 12:57:38 -0500

    Update ShoppingCartRepository.java

--- shoppingCartFolder/ShoppingCartRepository.java
+++ shoppingCartFolder/ShoppingCartRepository.java
@@ -2,6 +2,7 @@ package com.group_3.restful_group_3_project.shoppingcart;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
+import com.group_3.restful_group_3_project.shoppingCartFolder.ShoppingCart;
 
 public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
     Optional<ShoppingCart> findByUserID(String userID);
