1fb6c7c3b27a0f7bc4e9ba1e27e802ef16671875
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-07-06 14:41:11 -0500

    Update ShoppingCartController.java

--- shoppingCartFolder/ShoppingCartController.java
+++ shoppingCartFolder/ShoppingCartController.java
@@ -1,19 +1,13 @@
-package com.group_3.restful_group_3_project;
+package com.group_3.restful_group_3_project.shoppingCartFolder;
 
 import com.group_3.restful_group_3_project.exceptions.CartNotFoundException;
 import com.group_3.restful_group_3_project.exceptions.BookNotFoundException;
 import com.group_3.restful_group_3_project.services.ShoppingCartService;
-import com.group_3.restful_group_3_project.bookFolder.Book;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-
-import java.util.List;
 
 @RestController
-@RequestMapping("/shoppingcart")
+@RequestMapping("/cart")
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
@@ -43,10 +37,10 @@ public class ShoppingCartController {
         return shoppingCartService.getBooksInCart(userID);
     }
 
-    @DeleteMapping("/books")
-    public ResponseEntity<String> deleteBookFromCart(@RequestParam String userID, @RequestParam String bookID) {
+    @DeleteMapping("/removeBook")
+    public ResponseEntity<String> removeBookFromCart(@RequestParam String userID, @RequestParam String bookID) {
         try {
-            shoppingCartService.deleteBookFromCart(userID, bookID);
+            shoppingCartService.removeBookFromCart(userID, bookID);
             return ResponseEntity.ok("Book removed from cart successfully");
         } catch (CartNotFoundException | BookNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
