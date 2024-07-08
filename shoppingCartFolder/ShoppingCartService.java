b43d04e560a24058d38c5077d3f42b400677edcb
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-07-08 18:29:56 -0500

    Update ShoppingCartService.java

--- shoppingCartFolder/ShoppingCartService.java
+++ shoppingCartFolder/ShoppingCartService.java
@@ -1,70 +1,108 @@
-package com.group_3.restful_group_3_project.services;
+package com.group_3.restful_group_3_project.shoppingCartFolder;
+
 
 import com.group_3.restful_group_3_project.bookFolder.Book;
-import com.group_3.restful_group_3_project.exceptions.CartNotFoundException;
-import com.group_3.restful_group_3_project.exceptions.BookNotFoundException;
-import com.group_3.restful_group_3_project.shoppingCartFolder.ShoppingCart;
-import com.group_3.restful_group_3_project.shoppingCartFolder.ShoppingCartRepository;
 import com.group_3.restful_group_3_project.bookFolder.BookRepository;
+import com.group_3.restful_group_3_project.userFolder.User;
+import com.group_3.restful_group_3_project.userFolder.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class ShoppingCartService {
 
-    @Autowired
-    private ShoppingCartRepository shoppingCartRepository;
+private final BookRepository bookRepository;
+private final UserRepository userRepository;
 
-    @Autowired
-    private BookRepository bookRepository;
 
-    public double getSubtotalByUserID(String userID) {
-        Optional<ShoppingCart> cartOpt = shoppingCartRepository.findByUserID(userID);
-        if (cartOpt.isPresent()) {
-            ShoppingCart cart = cartOpt.get();
-            return cart.calculateSubtotal();
-        } else {
-            throw new CartNotFoundException("Cart not found for user ID: " + userID);
-        }
-    }
+@Autowired
+public ShoppingCartService(BookRepository bookRepository, UserRepository userRepository) {
+this.bookRepository = bookRepository;
+this.userRepository = userRepository;
+}
 
-    public void addBookToCart(String userID, String bookID) {
-        Optional<ShoppingCart> cartOpt = shoppingCartRepository.findByUserID(userID);
-        if (!cartOpt.isPresent()) {
-            throw new CartNotFoundException("Cart not found for user ID: " + userID);
-        }
 
-        Book book = findBookByID(bookID);
-        ShoppingCart cart = cartOpt.get();
-        cart.addBook(book);
-        shoppingCartRepository.save(cart);
-    }
+public double getSubtotalByUserID(String userID) {
+User user = userRepository.findByUserID(userID);
+if (user != null) {
+return user.getShoppingCart().getSubTot();
+}
+throw new RuntimeException("User not found");
+}
 
-    public List<Book> getBooksInCart(String userID) {
-        Optional<ShoppingCart> cartOpt = shoppingCartRepository.findByUserID(userID);
-        if (cartOpt.isPresent()) {
-            return cartOpt.get().getBooks();
-        } else {
-            throw new CartNotFoundException("Cart not found for user ID: " + userID);
-        }
-    }
 
-    public void deleteBookFromCart(String userID, String bookID) {
-        Optional<ShoppingCart> cartOpt = shoppingCartRepository.findByUserID(userID);
-        if (!cartOpt.isPresent()) {
-            throw new CartNotFoundException("Cart not found for user ID: " + userID);
-        }
 
-        ShoppingCart cart = cartOpt.get();
-        cart.removeBook(bookID);
-        shoppingCartRepository.save(cart);
-    }
+public void addBookToCart(String userID, String isbn){
+User user = userRepository.findByUserID(userID);
+if (user != null) {
+Book book = bookRepository.findByIsbn(isbn);
+if (book != null) {
+ShoppingCart shoppingCart = user.getShoppingCart();
+List<Book> cart_contents = shoppingCart.getBookList();
+cart_contents.add(book);
+Double newSubtototal = shoppingCart.getSubTot() + book.getPrice();
+shoppingCart.setSubTot(newSubtototal);
+userRepository.save(user);
+} else {
+throw new RuntimeException("Book not found");
+}
+} else {
+throw new RuntimeException("User not found");
+}
+}
 
-    private Book findBookByID(String bookID) {
-        return bookRepository.findById(bookID).orElseThrow(() -> new BookNotFoundException("Book not found for book ID: " + bookID));
-    }
+
+
+public List<Book> getBooksInCart(String userID) {
+User user = userRepository.findByUserID(userID);
+if (user != null){
+ShoppingCart cart = user.getShoppingCart();
+return cart.getBookList();
+}
+throw new RuntimeException("User not Found");
+}
+
+
+
+public void removeBookFromCart(String userID, String isbn){
+User user = userRepository.findByUserID(userID);
+if (user != null){
+ShoppingCart cart = user.getShoppingCart();
+if(cart != null) {
+List<Book> bookList = cart.getBookList();
+Book bookToRemove = null;
+for(Book book: bookList) {
+if(book.getIsbn().equals(isbn)) {
+bookToRemove = book;
+break;
+}
+}
+if(bookToRemove != null) {
+bookList.remove(bookToRemove);
+cart.setSubTot(calculateSubTotal(bookList));
+userRepository.save(user);
+} else {
+throw new RuntimeException("Book not found");
+}
+} else {
+throw new RuntimeException("Cart Empty");
+}
+} else {
+throw new RuntimeException("User not found");
+}
+}
+
+
+public double calculateSubTotal(List<Book> cart) {
+double total = 0;
+for(Book book: cart) {
+total += book.getPrice();
+}
+return total;
+}
 }
