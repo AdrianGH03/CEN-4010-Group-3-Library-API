81c900976368845d24c12901fadbba710c7e961f
Thalia <99617152+Lioness305@users.noreply.github.com>
2024-07-08 18:00:42 -0400

    Update BookRepository.java
    Added publisher

--- bookFolder/BookRepository.java
+++ bookFolder/BookRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository // Annotation to specify that this class is a repository, which connects to the database
 public interface BookRepository extends MongoRepository<Book, String> {
 	 Book findByIsbn(String isbn);
+         List<Book> findByPublisher(String publisher);
 }
