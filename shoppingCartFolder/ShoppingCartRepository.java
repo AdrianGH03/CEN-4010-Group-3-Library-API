2731eac6d6935eaa506a554519042a2bca3b7bc8
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-07-08 18:46:15 -0500

    Delete shoppingCartFolder/ShoppingCartRepository.java

--- shoppingCartFolder/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package shoppingcartfolder;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-import com.group_3.restful_group_3_project.shoppingCartFolder.ShoppingCart;
-
-public interface ShoppingCartRepository extends MongoRepository<ShoppingCart, String> {
-    Optional<ShoppingCart> findByUserID(String userID);
-}
