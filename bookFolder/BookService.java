d8210bc9b5cfec91c79bb52f57a47d846496eeac
Thalia <99617152+Lioness305@users.noreply.github.com>
2024-07-08 18:30:07 -0400

    Update BookService.java
    added list and publisher

--- bookFolder/BookService.java
+++ bookFolder/BookService.java
@@ -1,5 +1,5 @@
 package com.group_3.restful_group_3_project.bookFolder;
-
+import java.util.List;
 import java.time.LocalDate;
 import java.util.NoSuchElementException;
 
@@ -22,5 +22,16 @@ public class BookService {
 
     public Book getBookByIsbn(String isbn) {
         return bookRepository.findById(isbn).orElse(null);
+    }
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> getBooksByPublisher(String publisher) {
+        return bookRepository.findByPublisher(publisher);
+    }
+
+    public void updateBook(Book book) {
+        bookRepository.save(book);
     }
 }
