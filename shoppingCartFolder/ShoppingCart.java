7ddaf410577f5a0d9392697b539d216e777f514b
Fantasha Edwards <141473641+itsfantasha@users.noreply.github.com>
2024-07-24 22:02:41 -0500

    Update ShoppingCart.java

--- shoppingCartFolder/ShoppingCart.java
+++ shoppingCartFolder/ShoppingCart.java
@@ -1,24 +1,32 @@
 package com.group_3.restful_group_3_project.shoppingCartFolder;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import com.group_3.restful_group_3_project.bookFolder.Book;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Document(collection = "users")
-
-
 public class ShoppingCart {
-String userID;
-double subTot;
-List<Book> bookList = new ArrayList<>();
+    private List<Book> bookList;
+    private double subtotal;
 
+    public ShoppingCart() {
+        this.bookList = new ArrayList<>();
+        this.subtotal = 0.0;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
